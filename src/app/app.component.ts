c4525b328217e2e0c1b85db1f05762204e05ecad
frankxhunter <frankxhunter@gmail.com>
2024-07-03 12:25:45 +0200

    Creada la lista de tareas con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TasksService } from './services/tasks.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'List_Task';
+export class AppComponent implements OnInit {
+  
+  listTasks: string[] = [];
+  newTask = '';
+  
+  private _tasksServices = inject(TasksService)
+
+  ngOnInit(): void {
+    this.refresh()
+  }
+
+  refresh(){
+    this.listTasks = this._tasksServices.getTasks();
+  }
+  deleteTask(index: number){
+    this._tasksServices.deleteTask(index)
+    this.refresh();
+  }
+  addTask(){
+    if(this.newTask){
+      this._tasksServices.addTask(this.newTask)
+      this.newTask = ''
+      this.refresh();
+    }
+  }
+
 }
+
